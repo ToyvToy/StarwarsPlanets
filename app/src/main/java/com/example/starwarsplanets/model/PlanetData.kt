b2d0b0c8f064de0baef5657af277d8e42047f007
ToyvToy <toynorwood12@gmail.com>
2026-04-13 10:45:19 -0500

    Started DetailActivity

---
+++ app/src/main/java/com/example/starwarsplanets/model/PlanetData.kt
@@ -0,0 +1,25 @@
+package com.example.starwarsplanets.model
+
+class PlanetData {
+
+    var planetName: String? = null
+    var planetType: String? = null
+    val planetImage: String? = null
+    var planetDescription: String? = null
+
+    // Default constructor. Need this or app will crash.
+    constructor(){}
+
+    // Initializes planet data with specific values listed below.
+    constructor(
+        planetName: String?,
+        planetType: String?,
+        planetImage: String?,
+        planetDescription: String?
+    ) {
+        this.planetName = planetName
+        this.planetType = planetType
+        this.planetImage = planetImage
+        this.planetDescription = planetDescription
+    }
+}
