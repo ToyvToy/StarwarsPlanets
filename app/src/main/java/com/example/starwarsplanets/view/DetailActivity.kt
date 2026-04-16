3cd82de915a504dc74608cc54426d8558788c287
ToyvToy <toynorwood12@gmail.com>
2026-04-16 09:53:13 -0500

    Done

--- app/src/main/java/com/example/starwarsplanets/view/DetailActivity.kt
+++ app/src/main/java/com/example/starwarsplanets/view/DetailActivity.kt
@@ -3,11 +3,15 @@ package com.example.starwarsplanets.view
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.starwarsplanets.R
+import com.example.starwarsplanets.viewmodel.getProgressDrawable
+import com.example.starwarsplanets.viewmodel.loadImage
 
 class DetailActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,6 +26,27 @@ class DetailActivity : AppCompatActivity() {
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
         }
+
+        // View references.
+        val planetImage: ImageView = findViewById(R.id.planetImage)
+        val planetName: TextView = findViewById(R.id.planetName)
+        val planetType: TextView = findViewById(R.id.planetType)
+        val planetDescription: TextView = findViewById(R.id.planetDescription)
+
+        val planetNameDetail = intent.getStringExtra("planetName")
+        val planetTypeDetail = intent.getStringExtra("planetType")
+        val planetDescriptionDetail = intent.getStringExtra("planetDescription")
+        val planetImageDetail = intent.getStringExtra("planetImage")
+
+        planetName.text = planetNameDetail
+        planetType.text = planetTypeDetail
+        planetDescription.text = planetDescriptionDetail
+
+        planetImage.loadImage(
+            planetImageDetail,
+            getProgressDrawable(this)
+        )
+
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
